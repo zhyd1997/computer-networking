5a07d3feb64181a5ccfb91d9789412fcbf470b60
Yadong Zhang <zhyd007@gmail.com>
2023-02-23 15:35:51 +0800

    feat: added SMTP mail client. (#5)

--- 2.7/TCP/Client.py
+++ 2.7/TCP/Client.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from socket import *
-serverName = '0.0.0.0' # hostname after run 'Server.py'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+clientSocket.connect(('', serverPort))
 sentence = input('Input lowercase sentence:')
 clientSocket.send(sentence.encode())
 modifiedMessage = clientSocket.recv(2048)
